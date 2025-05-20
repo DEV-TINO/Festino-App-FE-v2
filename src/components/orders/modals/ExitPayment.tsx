e3b3d0d8b333674bbb53ee566cf3af4761adc91d
김민지 <alswlk0306@gmail.com>
2025-05-20 16:44:04 +0900

    fix: 모달 오류 수정

--- src/components/orders/modals/ExitPayment.tsx
+++ src/components/orders/modals/ExitPayment.tsx
@@ -11,39 +11,32 @@ const ExitPaymentModal: React.FC = () => {
 
   return (
     <div
-      className=" flex items-center  justify-center"
-      onClick={(e) => {
-        e.stopPropagation();
-      }}
+      className="relative col-start-2  row-start-2 bg-white rounded-3xl flex flex-col items-center px-10 py-8 gap-5 w-[320px] max-w-full"
+      onClick={(e) => e.stopPropagation()}
     >
-      <div
-        className="relative col-start-2  row-start-2 bg-white rounded-3xl flex flex-col items-center px-10 py-8 gap-5 w-[320px] max-w-full"
-        onClick={(e) => e.stopPropagation()}
-      >
-        <div className="w-12 h-12 bg-error rounded-full grid place-items-center">
-          <img src="/icons/commons/error.svg" />
-        </div>
-        <div className="w-full flex flex-col gap-3 items-center break-keep text-center">
-          <p className="text-secondary-700 text-xl font-bold">주문 퇴장</p>
-          <p className="text-secondary-500">주문을 그만두시겠습니까?</p>
-        </div>
-        <div className="flex w-full gap-3 font-bold">
-          <button
-            className="w-full h-11 rounded-full border-2 border-primary-700 text-primary-700"
-            onClick={() => closeModal()}
-          >
-            취소
-          </button>
-          <button
-            className="w-full h-11 rounded-full text-white bg-primary-700"
-            onClick={() => {
-              closeModal();
-              navigate(`/order/${boothId}/${tableNum}`);
-            }}
-          >
-            확인
-          </button>
-        </div>
+      <div className="w-12 h-12 bg-error rounded-full grid place-items-center">
+        <img src="/icons/commons/error.svg" />
+      </div>
+      <div className="w-full flex flex-col gap-3 items-center break-keep text-center">
+        <p className="text-secondary-700 text-xl font-bold">주문 퇴장</p>
+        <p className="text-secondary-500">주문을 그만두시겠습니까?</p>
+      </div>
+      <div className="flex w-full gap-3 font-bold">
+        <button
+          className="w-full h-11 rounded-full border-2 border-primary-700 text-primary-700"
+          onClick={() => closeModal()}
+        >
+          취소
+        </button>
+        <button
+          className="w-full h-11 rounded-full text-white bg-primary-700"
+          onClick={() => {
+            closeModal();
+            navigate(`/order/${boothId}/${tableNum}`);
+          }}
+        >
+          확인
+        </button>
       </div>
     </div>
   );
