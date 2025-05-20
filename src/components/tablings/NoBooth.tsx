93fdd8392437b720f69e0fedc57240b8aa323f31
김민지 <alswlk0306@gmail.com>
2025-05-20 14:15:22 +0900

    style: 사이즈 줄이기

--- src/components/tablings/NoBooth.tsx
+++ src/components/tablings/NoBooth.tsx
@@ -1,9 +1,9 @@
 const NoBooth: React.FC = () => {
   return (
     <>
-      <div className="w-full h-full flex flex-col items-center gap-2 py-2">
-        <p className="text-xl font-medium">예약 가능한 부스가 없습니다.</p>
-        <div className="bg-error-full bg-cover bg-center bg-no-repeat w-full aspect-[35/31]"></div>
+      <div className="p-10 w-full h-60 flex flex-col items-center justify-center gap-2 py-2">
+        <div className="bg-error-full bg-cover bg-center bg-no-repeat w-1/2 aspect-[35/31]"></div>
+        <p className="text-xl text-gray-500 pt-2">예약 가능한 부스가 없어요...</p>
       </div>
     </>
   );
