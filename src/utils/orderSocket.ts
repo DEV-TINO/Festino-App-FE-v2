4d3ba0c4115699977f5bc57362fda1aa3434b6b1
김민지 <alswlk0306@gmail.com>
2025-05-21 15:33:22 +0900

    fix: url 수정

--- src/utils/orderSocket.ts
+++ src/utils/orderSocket.ts
@@ -10,7 +10,7 @@ export const connectOrderSocket = (boothId: string, tableNum: number) => {
   if (client && client.connected) return;
 
   const newClient = new Client({
-    brokerURL: `wws://${import.meta.env.VITE_API_URL}`,
+    brokerURL: `wss://${import.meta.env.VITE_API_URL}`,
     reconnectDelay: 5000,
     onConnect: (frame) => {
       const sessionId = frame.headers['user-name'];
