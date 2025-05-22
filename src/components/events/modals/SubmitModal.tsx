e1b8799a86070f94bce398f90c224c78f3e5db48
jzongyun <jeong.yun@tukorea.ac.kr>
2025-05-22 15:18:12 +0900

    style: 정답 제출 모달 css style 변경

--- src/components/events/modals/SubmitModal.tsx
+++ src/components/events/modals/SubmitModal.tsx
@@ -1,5 +1,6 @@
 import useBaseModal from "@/stores/baseModal";
 import useEventStore from "@/stores/events/eventStore";
+import { truncateText } from "@/utils/utils";
 
 const SubmitModal: React.FC = () => {
   const { closeModal } = useBaseModal();
@@ -15,11 +16,12 @@ const SubmitModal: React.FC = () => {
           <img src="/icons/commons/info.svg" />
         </div>
         <div className="w-full flex flex-col gap-3 items-center">
-          <p className="text-secondary-500">제출되었습니다</p>
+          <p className="text-secondary-700 text-xl font-bold">제출 완료</p>
+          <p className="text-secondary-500">제출이 완료되었습니다</p>
         </div>
-        <div className="flex flex-col items-center">
-          <p>제출 답안</p>
-          <p className="text-secondary-500">{ answer }</p>
+        <div className="flex gap-2 items-center">
+          <p>제출 답안 :</p>
+          <p className="text-secondary-500 line-break">{ truncateText(answer, 12) }</p>
         </div>
         <button
           className="w-full h-12 bg-primary-900 rounded-3xl text-white font-semibold text-xl"
