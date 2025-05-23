4fa2f54ec498e96ae7adf488bc4cf16a6a1c0613
김민지 <alswlk0306@gmail.com>
2025-05-23 20:28:15 +0900

    style: 티노 이미지 추가

--- src/pages/orders/ReTryQRPage.tsx
+++ src/pages/orders/ReTryQRPage.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 const RetryQRPage: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-white text-black text-center px-6">
-      <div className="mb-8">
+      <div className="w-48 h-[168px] aspect-auto bg-error-full bg-cover"></div>
+      <div className="mt-8">
         <h1 className="text-3xl font-extrabold mb-2">QR을 다시 촬영해 주세요</h1>
         <p className="text-m text-gray-600">테이블에 부착된 QR 코드를 다시 촬영해 주세요</p>
       </div>
