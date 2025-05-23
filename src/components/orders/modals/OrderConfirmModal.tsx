e8e334bbc84987fc67f36ffca3b7fc8a9b2a6298
김민지 <alswlk0306@gmail.com>
2025-05-23 17:13:44 +0900

    fix: 카카오 아이콘 이미지 비율 수정

--- src/components/orders/modals/OrderConfirmModal.tsx
+++ src/components/orders/modals/OrderConfirmModal.tsx
@@ -165,7 +165,7 @@ const OrderConfirmModal: React.FC = () => {
               rel="noopener noreferrer"
               className="flex flex-col items-center justify-center w-[120px] h-[80px] rounded-xl border border-gray-200"
             >
-              <img src="/icons/orders/kakao-pay.png" alt="카카오페이" className="w-8 h-8" />
+              <img src="/icons/orders/kakao-pay.png" alt="카카오페이" className="w-[82px] h-8" />
               <span className="text-xs mt-1 select-none">카카오페이</span>
             </a>
           )}
