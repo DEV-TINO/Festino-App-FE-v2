5f92cf7531ecfa37243d09bec0d841714b3e15a3
jzongyun <jeong.yun@tukorea.ac.kr>
2025-05-26 03:08:39 +0900

    test: 아날리틱스 qr 확인 테스트

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,8 +1,26 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Footer from '@/components/footers/Footer';
 import NavTap from '@/components/headers/NavTap';
 import { Outlet } from 'react-router-dom';
+import ReactGA from 'react-ga4';
 
 const MainLayout: React.FC = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const qr = params.get('qr');
+
+    if (qr) {
+      ReactGA.event({
+        category: 'QR_Code',
+        action: 'qr_entry',
+        label: qr,
+      });
+    }
+  }, []);
+
   return (
     <>
       <Outlet />
