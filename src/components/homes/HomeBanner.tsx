503a7fb9df4757c3038cfdfb0a9b2cd8642812a7
Hyochan <hyochan02@gmail.com>
2025-09-03 00:03:17 +0900

    design: 오탈자 수정

--- src/components/homes/HomeBanner.tsx
+++ src/components/homes/HomeBanner.tsx
@@ -5,7 +5,7 @@ const HomeBanner: React.FC = () => {
         <div className="absolute top-10 right-3.5 w-auto h-auto">
           <div className="flex flex-col items-end">
             <div className="font-blackhansans text-xs text-white  drop-shadow-banner-text whitespace-pre">
-              Festion와 함께하는 2025년{'  '}
+              Festino와 함께하는 2025년{'  '}
             </div>
             <div className="leading-tight font-blackhansans text-3xl text-white  drop-shadow-banner-text text-transparent to-100% bg-clip-text text-right">
               한국공대 동아리 홍보전
